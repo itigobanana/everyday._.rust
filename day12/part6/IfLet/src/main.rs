f87005c99afcccd0acb08dc2b68c09137d14d652
itigobanana <e255718@ie.u-ryukyu.ac.jp>
2026-05-09 22:11:44 +0900

    さぼった！！^^;

---
+++ day12/part6/IfLet/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let config_max = Some(3u8);
+    match config_max{
+        Some(max) => println!("The maximum is configured to be {}",max)
+        _ => (),
+    }
+}
+
+
+fn main(){
+    let config_max = Some(3u8);
+    if let Some(max) = config_max{
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
+
+fn main(){
+    let mut count = 0;
+    match coin{
+        Coin::Quarter(state) => println!("State quarter from {:?}", state);
+        _ => count += 1,
+    }
+}
+
+
+fn main(){
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin{
+        println!("State quarter from {;?}", state);
+    }else{
+        count += 1;
+    }
+}
